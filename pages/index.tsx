7d886a0167dc3e1fc964666525c865eddad83ecb
Evgeniy Dubskiy <e.dubskiy@gmail.com>
2022-03-21 01:16:40 +0300

    Moved review form to self contained folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { ReviewForm } from '../components/review-form.component'
+import { ReviewForm } from '../components/review-form/review-form.component'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
